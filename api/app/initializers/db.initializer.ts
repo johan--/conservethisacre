42aa0319ed925911247a8b099259a4c9aea0661b
Demid Nikitin <demidn@gmail.com>
2017-12-01 16:02:18 +0300

    added debug

--- api/app/initializers/db.initializer.ts
+++ api/app/initializers/db.initializer.ts
@@ -6,6 +6,7 @@ import { environment } from '../../environments/environment';
 export const databaseInitializer = (app: Application) => {
 
   // TODO: temporary <any> here. something wrong with database.type matching
+  console.log('\n\n\nConnecting to db:', environment.database);
   createConnection(<any>environment.database).then(connection => {
     console.log('Database initialized');
   }).catch(error => console.log(error));

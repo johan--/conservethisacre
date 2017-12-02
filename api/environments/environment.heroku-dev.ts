96d49b32e3c4a36123c3e95ab96b93593dbc387d
Demid Nikitin <demidn@gmail.com>
2017-12-03 02:50:52 +0300

    updated enviroment for heroku build

--- api/environments/environment.heroku-dev.ts
+++ api/environments/environment.heroku-dev.ts
@@ -3,6 +3,13 @@ import { Forest } from '../app/entities/forest';
 import { Parcel } from '../app/entities/parcel';
 
 export const environment = {
+  facebook : {
+    appId : '573983026276728',
+    apiVersion : 'v2.9',
+    apiURL : 'https://graph.facebook.com',
+    appSecret: '0420ab38063d84e8a77864612dd8485b'
+  },
+
   database: {
     type: 'mysql',
     url: DATABASE_URL,

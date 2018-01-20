5e56de7881faedb1c05d7285c1c84b9a0356b4d8
Demid Nikitin <demidn@gmail.com>
2018-01-20 05:13:28 +0300

    added example file that sets env variables

---
+++ env.example.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export CLEARDB_DATABASE_URL='mysql://username:passwrod@localhost:3306/database'
+export S3_BUCKET=''
+export AWS_ACCESS_KEY_ID=''
+export AWS_SECRET_ACCESS_KEY=''
+export S3_REGION=''
+export STRIPE_PUBLIC_API_KEY=''
+export STRIPE_SECRET_API_KEY=''

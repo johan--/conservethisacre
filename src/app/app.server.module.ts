04eb32c43445dd1a6f51ebd28cf8570332382877
Demid Nikitin <demidn@gmail.com>
2018-01-20 06:16:29 +0300

    added module map loader (error with no factories for lazy routes)

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 
 @NgModule({
   imports: [
     AppModule,
-    ServerModule
+    ServerModule,
+    ModuleMapLoaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
